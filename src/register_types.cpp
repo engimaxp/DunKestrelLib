5c42c723a1fee3567fec36d259429d9049ff714f
enigmaxp <wxf_072445@126.com>
2023-08-18 19:00:02 +0800

    1

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -4,6 +4,8 @@
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
+#include "example.h"
+
 using namespace godot;
 
 void initialize_gdextension_types(ModuleInitializationLevel p_level)
@@ -11,6 +13,12 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
+
+	ClassDB::register_class<ExampleRef>();
+	ClassDB::register_class<ExampleMin>();
+	ClassDB::register_class<Example>();
+	ClassDB::register_class<ExampleVirtual>(true);
+	ClassDB::register_abstract_class<ExampleAbstract>();
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
@@ -22,7 +30,7 @@ void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
 extern "C"
 {
 	// Initialization
-	GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT dunkestrel_lib(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 		init_obj.register_initializer(initialize_gdextension_types);
