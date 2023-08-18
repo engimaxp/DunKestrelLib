9fc057e88c547e602fb64ac517af12ae3b51d970
enigmaxp <wxf_072445@126.com>
2023-08-18 20:58:54 +0800

    2

---
+++ demo/node_2d.gd
@@ -0,0 +1,16 @@
+extends Node2D
+#
+#func _ready():
+#	var x = ExampleRef.new()
+#	print(x.id)
+@onready var label = $Label
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		test()
+		
+func test():
+	var x = ExampleRef.new()
+	label.text = str(Example.test_static(2,4))
+	print(Example.test_static(2,4))
+	print(x.id)
