024042151beda2339f59eb364b76cab47df3a5fe
enigmaxp <wxf_072445@126.com>
2023-09-02 09:05:48 +0800

    1

--- demo/node_2d.gd
+++ demo/node_2d.gd
@@ -14,7 +14,18 @@ func test():
 	var x = ExampleRef.new()
 	label.text = str(Example.test_static(2,4))
 	print(Example.test_static(2,4))
-	print(Example.test_static2(1))
+	var start_time = Time.get_ticks_usec()
+	print(Example.test_static2(2,[1,2,3,4,5],[
+		Vector3i(1,2,5),
+		Vector3i(1,3,2),
+		Vector3i(3,2,2),
+		Vector3i(3,4,5),
+		Vector3i(2,4,4),
+		Vector3i(2,5,2),
+		Vector3i(4,5,3),
+	]))
+	var end_time = Time.get_ticks_usec()
+	print("%f us" % (end_time - start_time))
 	print(example.test_array())
 	print("example.varargs_func_nv" ,example.varargs_func_nv("1","2","3"))
 	print(x.id)
